4d7891e63c731f77d8c97621edab4520fe059df6
Manuel Pires Luis <166626626+ManuelPiresLuis01@users.noreply.github.com>
2025-03-07 20:06:50 +0000

    Update conection.js

--- BACKEND/src/app/database/conection/conection.js
+++ BACKEND/src/app/database/conection/conection.js
@@ -1,20 +1,22 @@
 import mysql from "mysql"
-import dotenv from "dotenv"
+import dotenv from 'dotenv';
 
-dotenv.config()
+dotenv.config();
 
 const CONECTION = mysql.createConnection({
     host: process.env.HOSTNAME,
-    port: process.env.DBPORT,
+    port: Number(process.env.DBPORT),
     user: process.env.DBUSER,
     password: process.env.DBPASSWORD,
     database: process.env.DBNAME
 })
 
-CONECTION.connect((error) => {
-    if (error) {
-        console.error(error)
-    }
+CONECTION.connect((err) => {
+  if (err) {
+    console.error('Erro ao conectar:', err);
+    return;
+  }
+  console.log('Conectado ao MySQL do InfinityFree!');
 });
 
 export default CONECTION
