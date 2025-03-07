375182f01d250dbeb24db1fdfed52a9e8eded6d0
Manuel Pires Luis <166626626+ManuelPiresLuis01@users.noreply.github.com>
2025-03-07 18:48:11 +0000

    Update vite.config.js

--- FRONTEND/vite.config.js
+++ FRONTEND/vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: "0.0.0.0",
+    port: process.env.PORT ? parseInt(process.env.PORT) : 3000,
+    strictPort: true,
+    allowedHosts: ["PontoDeEncontro.onrender.com"],
+  },
+});
